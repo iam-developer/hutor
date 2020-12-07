12d2a9637b045c1805d9f6bf8fe2254dfbfe92db
iam-developer <turgunbaaev@gmail.com>
2020-12-07 23:28:30 +0300

    wefff

--- js/main.js
+++ js/main.js
@@ -42,8 +42,8 @@ $('.center--slider').slick({
       {
         breakpoint: 767,
         settings: {
-          prevArrow:"<div><img class='left__arrow' src='./hutor/img/left__arrow.png'></div>",
-          nextArrow:"<div><img class='right__arrow' src='./hutor/img/right__arrow.png'></div>",
+          prevArrow:"<img class='left__arrow' src='../hutor/img/left__arrow.png'>",
+          nextArrow:"<img class='right__arrow' src='../hutor/img/right__arrow.png'>",
           dots: false
         }
       }
